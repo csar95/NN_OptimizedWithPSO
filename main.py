cf759db596db84904e11c91e9e6f0f5b09e9986a
Tgl70 <bruceketta70@yahoo.it>
2019-10-02 13:56:17 +0100

    Almost finished the NeuralNetwork

--- main.py
+++ main.py
@@ -34,6 +34,16 @@ def draw_graph(j):
 
 ##################################################################
 
+nn = NeuralNetwork()
+nn.add(3, input_shape=2, activation='sigmoid')
+nn.add(4, activation='hyperbolic_tangent')
+nn.add(1, activation='gaussian')
+output = nn.feed_forward([[2], [3]])
+print("Output: ", output[0][0])
+
+
+
+'''
 nn = NeuralNetwork([1, 3, 4, 1])
 
 dataFile = 'Data/1in_linear.txt'
@@ -66,8 +76,9 @@ predicted_data = fig.add_subplot(1, 1, 1)
 
 step = 0
 
-animation = ani.FuncAnimation(fig, draw_graph, interval=2, cache_frame_data=False)
+animation = ani.FuncAnimation(fig, draw_graph, interval=2)
 
 plt.xticks([])
 plt.yticks([])
 plt.show()
+'''
--- neural_network.py
+++ neural_network.py
@@ -1,93 +1,94 @@
 import numpy as np
-import math
 
 
 class NeuralNetwork:
 
     learningRate = .1
 
-    def __init__(self, layers):
-        self.layersSize = np.array(layers)
-        self.weightMatrices = []
-        self.biases = []
+    # It creates an empty NN unless you give it the Matrix
+    def __init__(self):
+        self.layers_weights = []
+        self.layers_biases = []
+        self.layers_activation = []
 
-        for i in range(1, self.layersSize.size):
-            weights = np.random.uniform(-1, 1, size=(self.layersSize[i], self.layersSize[i-1]))
-            self.weightMatrices.append(weights)
-            bias = np.random.uniform(-1, 1, size=(self.layersSize[i], 1))
-            self.biases.append(bias)
+    # activation can be: {null, sigmoid, hyperbolic_tangent, cosine, gaussian}
+    def add(self, neurons, input_shape=None, activation='sigmoid'):
+        if not self.layers_weights:
+            matrix = np.random.uniform(-1, 1, size=(neurons, input_shape))
+        else:
+            #                                               Number of rows in the previous layer
+            matrix = np.random.uniform(-1, 1, size=(neurons, self.layers_weights[-1][:, 0].size))
+        bias = np.random.uniform(-1, 1, size=(neurons, 1))
+        self.layers_weights.append(matrix)
+        self.layers_biases.append(bias)
+        self.layers_activation.append(activation)
 
-    def feedforward(self, inputs):
-        layerInputs = np.reshape(inputs, newshape=(-1, 1))
+    def set_weights(self, pso_array):
+        pass
 
-        for i in range(1, self.layersSize.size):
-            # Calculate layer outputs
-            layerOutputs = np.dot(self.weightMatrices[i-1], layerInputs)
-            layerOutputs = np.add(layerOutputs, self.biases[i-1])
+    def feed_forward(self, inputs):
+        layer_inputs = inputs
+        for i in range(len(self.layers_weights)):
+            # Calculating the layer outputs
+            layer_outputs = np.dot(self.layers_weights[i], layer_inputs)
+            layer_outputs = np.add(layer_outputs, self.layers_biases[i])
             # Activation function
-            self.__map_layer(layerOutputs, self.layersSize[i])
-            layerInputs = layerOutputs  # This layer's outputs will be the inputs for the next layer
+            if self.layers_activation[i] == 'null':
+                self.map(self.null, layer_outputs)
+            elif self.layers_activation[i] == 'sigmoid':
+                self.map(self.sigmoid, layer_outputs)
+            elif self.layers_activation[i] == 'hyperbolic_tangent':
+                self.map(self.hyperbolic_tangent, layer_outputs)
+            elif self.layers_activation[i] == 'cosine':
+                self.map(self.cosine, layer_outputs)
+            elif self.layers_activation[i] == 'gaussian':
+                self.map(self.gaussian, layer_outputs)
+            # This layer's outputs will be the inputs for the next layer
+            layer_inputs = layer_outputs
+        return layer_outputs
 
-        return layerOutputs
+    @staticmethod
+    def map(func, inputs):
+        for i in range(inputs.size):
+            for j in range(inputs[i].size):
+                inputs[i][j] = func(inputs[i][j])
 
-    def feedforward_for_training(self, inputs):
-        layersInputs = []
-        layerInputs = np.reshape(inputs, newshape=(-1, 1))
-        layersInputs.append(layerInputs)
+    @staticmethod
+    def null(x):
+        return 0
 
-        for i in range(1, self.layersSize.size):
-            # Calculate layer outputs
-            layerOutputs = np.dot(self.weightMatrices[i-1], layersInputs[-1])
-            layerOutputs = np.add(layerOutputs, self.biases[i-1])
-            # Activation function
-            self.__map_layer(layerOutputs, self.layersSize[i])
-            layersInputs.append(layerOutputs)  # This layer's outputs will be the inputs for the next layer
-
-        return layersInputs
-
-    def train(self, inputs, targets):
-        layersOutputs = self.feedforward_for_training(inputs)
-        errors = np.array([])
-
-        for i in range(1, len(layersOutputs)):
-            if i == 1:
-                # Output errors
-                targets = np.reshape(targets, newshape=(-1, 1))
-                errors = np.subtract(targets, layersOutputs[-i])
-            else:
-                # Errors of a hidden layer
-                w = np.transpose(self.weightMatrices[-(i-1)])
-                errors = np.dot(w, errors)
-
-            ########### Calculate gradient: Layer(i) -> Layer(i+1) ###########
-            self.__map_layer_derivative(layersOutputs[-i], self.layersSize[-i])
-            gradients = np.dot(self.learningRate,
-                               np.multiply(layersOutputs[-i], errors))  # np.multiply -> Hadamard product
-
-            ########### Update weights: Layer(i) -> Layer(i+1) ###########
-            if i == len(layersOutputs) - 1:
-                layer_T = np.reshape(inputs, newshape=(1, -1))
-            else:
-                layer_T = np.transpose(layersOutputs[-(i+1)])
-
-            w_variation = np.dot(gradients, layer_T)
-            self.weightMatrices[-i] = np.add(self.weightMatrices[-i], w_variation)
-
-            ########### Adjust bias. The variation is just the gradients ###########
-            self.biases[-i] = np.add(self.biases[-i], gradients)
-
-    def __map_layer(self, layer, size):
-        for i in range(size):
-            layer[i][0] = self.sigmoid(layer[i][0])
-
-    def __map_layer_derivative(self, layer, size):
-        for i in range(size):
-            layer[i][0] = self.derivative_sigmoid(layer[i][0])
+    @staticmethod
+    def derivative_null(x):
+        return 0
 
     @staticmethod
     def sigmoid(x):
-        return 1 / (1 + math.exp(-x))
+        return 1 / (1 + np.exp(-x))
 
     @staticmethod
-    def derivative_sigmoid(y):
-        return y * (1 - y)
+    def derivative_sigmoid(x):
+        return x * (1 - x)
+
+    @staticmethod
+    def hyperbolic_tangent(x):
+        return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+    @staticmethod
+    def derivative_hyperbolic_tangent(x):
+        return 1 - (x**2)
+
+    @staticmethod
+    def cosine(x):
+        return np.cos(x)
+
+    @staticmethod
+    def derivative_cosine(x):
+        return -np.sin(x)
+
+    @staticmethod
+    def gaussian(x):
+        return np.exp(-(x**2))
+
+    @staticmethod
+    def derivative_gaussian(x):
+        return -2 * x * np.exp(-(x**2))
