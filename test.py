03f39d3fb65603aff56ed8793cb3de825f421c6f
Csar Gutirrez Carrero <cg93@hw.ac.uk>
2019-11-16 18:22:49 +0000

    Experiments preparation.

--- test.py
+++ test.py
@@ -10,24 +10,18 @@ populationSize = [10, 20, 30, 40, 50, 60]
 neighborhood = [2, 3, 4, 5, 6, 7]
 layers = [1, 2, 3, 4]
 
+popSizeError = []
+popSizeTime = []
 
-for data in dataset:
-    dataFile = data
+neighborhoodError = []
+neighborhoodTime = []
 
-
-
+layersError = []
+layersTime = []
 
 ########################## ALG. INITIALIZATION ##########################
 
-# dataFile = 'Data/1in_cubic.txt'
 dataFile = 'Data/1in_linear.txt'
-# dataFile = 'Data/1in_sine.txt'
-# dataFile = 'Data/1in_tanh.txt'
-# dataFile = 'Data/2in_complex.txt'
-# dataFile = 'Data/2in_xor.txt'
-
-
-
 
 file = open(dataFile, 'rt')
 
@@ -41,78 +35,221 @@ for line in file:
     X_train = np.append(X_train, [np.array(coordinates[:(dim-1)], dtype=float)], axis=0)
     y_train = np.append(y_train, [float(coordinates[dim-1])])
 
-aux = np.column_stack((X_train, y_train))
-
-# TODO: try from 1 to 5
-nn = NeuralNetwork()
-nn.add(4, input_shape=(dim-1))
-nn.add(3)
-nn.add(1)
-
-pso = PSO(nn)
-
-pso.generate_initial_population()
-
-############################ GENERATE FIGURE ############################
-
-step = 0
 max_steps = 750
-min_error = 1.
-time_step = []
-error_historical = []
-
 alfa1 = .9
 alfa2 = .4
 
-time1 = .0
+#########################################################################
 
-time_step.append([step])
+print('-------------------------------------------------------------')
 
-if step % 50 == 0:
-    print(f'Step: {step} - Minimum error found so far: {min_error}')
+for popSize in populationSize:
 
-inertia_weight = (alfa1 - alfa2) * ((max_steps - step) / max_steps) + alfa2
+    avgError = .0
+    avgTime = .0
 
-# Testing how good the parameters are with part of the data after randomizing it
-# This helps to discover new configurations for the NN
-np.random.shuffle(aux)
-X = np.array([np.array(elem[:(dim - 1)]) for elem in aux[:30]])
-y = np.array([elem[dim - 1] for elem in aux[:30]])
+    for i in range(10):
 
-# Calculate fitness of each particle
-for particle in pso.population:
-    particle.calculate_fitness(nn, X, y)
+        aux = np.column_stack((X_train, y_train))
 
-# Choose the particle with the best fitness value of all as gBest for each particle
-# (Reference slides or idea taken from https://www.sciencedirect.com/science/article/pii/S0020025517306485)
-if step < max_steps / 3:
-    pso.update_neighborhood_best_random()  # Exploration
-else:
-    pso.update_neighborhood_best_local()  # Exploitation
+        nn = NeuralNetwork()
+        nn.add(4, input_shape=(dim-1))
+        nn.add(3)
+        nn.add(1)
 
-# Update velocity and position for each particle
-for particle in pso.population:
-    particle.update_velocity(inertia_weight, pso.importancePBest, pso.importanceGBest)
-    particle.update_position()
+        pso = PSO(nn, populationSize=popSize)
+        pso.generate_initial_population()
 
-global_best = pso.find_global_best()
+        min_error = 1.
 
-best_error = global_best.pBestFitness
-error_historical.append(best_error)
-min_error = best_error if best_error < min_error else min_error
+        time1 = time.time()
 
-nn.set_parameters(global_best.pBest)
-y_preds = np.array([])
-for x in X_train:
-    y_pred = nn.feed_forward(np.array(x))[0]
-    y_preds = np.append(y_preds, y_pred)
+        for step in range(max_steps):
+            inertia_weight = (alfa1 - alfa2) * ((max_steps - step) / max_steps) + alfa2
 
-if step == 0:
-    time1 = time.time()
+            # Testing how good the parameters are with part of the data after randomizing it
+            # This helps to discover new configurations for the NN
+            np.random.shuffle(aux)
+            X = np.array([np.array(elem[:(dim - 1)]) for elem in aux[:30]])
+            y = np.array([elem[dim - 1] for elem in aux[:30]])
 
-if step == max_steps:
-    time2 = time.time()
-    print(time2 - time1)
-# plt.savefig(str(step) + '.png')
+            # Calculate fitness of each particle
+            for particle in pso.population:
+                particle.calculate_fitness(nn, X, y)
 
-step += 1
+            # Choose the particle with the best fitness value of all as gBest for each particle
+            # (Reference slides or idea taken from https://www.sciencedirect.com/science/article/pii/S0020025517306485)
+            if step < max_steps / 3:
+                pso.update_neighborhood_best_random()  # Exploration
+            else:
+                pso.update_neighborhood_best_local()  # Exploitation
+
+            # Update velocity and position for each particle
+            for particle in pso.population:
+                particle.update_velocity(inertia_weight, pso.importancePBest, pso.importanceGBest)
+                particle.update_position()
+
+            global_best = pso.find_global_best()
+
+            best_error = global_best.pBestFitness
+            min_error = best_error if best_error < min_error else min_error
+
+        timeElapsed = time.time() - time1
+        print(f'Episode: {i} | Minimum error: {min_error} | Time: {timeElapsed}')
+
+        avgError += min_error
+        avgTime += timeElapsed
+
+    avgError /= 10
+    avgTime /= 10
+
+    popSizeError.append(avgError)
+    popSizeTime.append(avgTime)
+    print(f'\nPopulation size: {popSize} | Average error: {avgError} | Average time: {avgTime}\n')
+
+#########################################################################
+
+print('-------------------------------------------------------------')
+
+for n in neighborhood:
+
+    avgError = .0
+    avgTime = .0
+
+    for i in range(10):
+
+        aux = np.column_stack((X_train, y_train))
+
+        nn = NeuralNetwork()
+        nn.add(4, input_shape=(dim-1))
+        nn.add(3)
+        nn.add(1)
+
+        pso = PSO(nn, neighborhood=n)
+        pso.generate_initial_population()
+
+        min_error = 1.
+
+        time1 = time.time()
+
+        for step in range(max_steps):
+            inertia_weight = (alfa1 - alfa2) * ((max_steps - step) / max_steps) + alfa2
+
+            # Testing how good the parameters are with part of the data after randomizing it
+            # This helps to discover new configurations for the NN
+            np.random.shuffle(aux)
+            X = np.array([np.array(elem[:(dim - 1)]) for elem in aux[:30]])
+            y = np.array([elem[dim - 1] for elem in aux[:30]])
+
+            # Calculate fitness of each particle
+            for particle in pso.population:
+                particle.calculate_fitness(nn, X, y)
+
+            # Choose the particle with the best fitness value of all as gBest for each particle
+            # (Reference slides or idea taken from https://www.sciencedirect.com/science/article/pii/S0020025517306485)
+            if step < max_steps / 3:
+                pso.update_neighborhood_best_random()  # Exploration
+            else:
+                pso.update_neighborhood_best_local()  # Exploitation
+
+            # Update velocity and position for each particle
+            for particle in pso.population:
+                particle.update_velocity(inertia_weight, pso.importancePBest, pso.importanceGBest)
+                particle.update_position()
+
+            global_best = pso.find_global_best()
+
+            best_error = global_best.pBestFitness
+            min_error = best_error if best_error < min_error else min_error
+
+        timeElapsed = time.time() - time1
+        print(f'Episode: {i} | Minimum error: {min_error} | Time: {timeElapsed}')
+
+        avgError += min_error
+        avgTime += timeElapsed
+
+    avgError /= 10
+    avgTime /= 10
+
+    neighborhoodError.append(avgError)
+    neighborhoodTime.append(avgTime)
+
+    print(f'\nNum. of neighbors: {n} | Average error: {avgError} | Average time: {avgTime}\n')
+
+#########################################################################
+
+print('-------------------------------------------------------------')
+
+for nLayers in layers:
+
+    avgError = .0
+    avgTime = .0
+
+    for i in range(10):
+
+        aux = np.column_stack((X_train, y_train))
+
+        nn = NeuralNetwork()
+        nn.add(4, input_shape=(dim-1))
+        for _ in range(nLayers):
+            nn.add(3)
+        nn.add(1)
+
+        pso = PSO(nn)
+        pso.generate_initial_population()
+
+        min_error = 1.
+
+        time1 = time.time()
+
+        for step in range(max_steps):
+            inertia_weight = (alfa1 - alfa2) * ((max_steps - step) / max_steps) + alfa2
+
+            # Testing how good the parameters are with part of the data after randomizing it
+            # This helps to discover new configurations for the NN
+            np.random.shuffle(aux)
+            X = np.array([np.array(elem[:(dim - 1)]) for elem in aux[:30]])
+            y = np.array([elem[dim - 1] for elem in aux[:30]])
+
+            # Calculate fitness of each particle
+            for particle in pso.population:
+                particle.calculate_fitness(nn, X, y)
+
+            # Choose the particle with the best fitness value of all as gBest for each particle
+            # (Reference slides or idea taken from https://www.sciencedirect.com/science/article/pii/S0020025517306485)
+            if step < max_steps / 3:
+                pso.update_neighborhood_best_random()  # Exploration
+            else:
+                pso.update_neighborhood_best_local()  # Exploitation
+
+            # Update velocity and position for each particle
+            for particle in pso.population:
+                particle.update_velocity(inertia_weight, pso.importancePBest, pso.importanceGBest)
+                particle.update_position()
+
+            global_best = pso.find_global_best()
+
+            best_error = global_best.pBestFitness
+            min_error = best_error if best_error < min_error else min_error
+
+        timeElapsed = time.time() - time1
+        print(f'Episode: {i} | Minimum error: {min_error} | Time: {timeElapsed}')
+
+        avgError += min_error
+        avgTime += timeElapsed
+
+    avgError /= 10
+    avgTime /= 10
+
+    layersError.append(avgError)
+    layersTime.append(avgTime)
+
+    print(f'\nNum. of layers: {nLayers} | Average error: {avgError} | Average time: {avgTime}\n')
+
+print('------------------------------------------------------------- ')
+print(f'Population size error: {popSizeError}')
+print(f'Population size time: {popSizeTime}')
+print(f'Number of informants error: {neighborhoodError}')
+print(f'Number of informants time: {neighborhoodTime}')
+print(f'Number of layers error: {layersError}')
+print(f'Number of layers time: {layersTime}')
